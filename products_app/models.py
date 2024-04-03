0524bff3ebff6adbd24b0c9f087cdf4e7f401ff9
jonathanespana <tempe.espana@gmail.com>
2024-04-02 18:24:32 -0600

    added model manager and updated product detail and product list views

--- products_app/models.py
+++ products_app/models.py
@@ -15,11 +15,20 @@ def upload_image_path(instance, filename):
     final_filename = f'{new_filename}{ext}'
     return f'products/{new_filename}/{final_filename}'
 
+class ProductManager(models.Manager):
+    def get_by_id(self, pk):
+        qs = self.get_queryset().filter(pk=pk)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     price= models.DecimalField(decimal_places=2, max_digits=10, default=39.99)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.name
--- products_app/views.py
+++ products_app/views.py
@@ -7,9 +7,12 @@ from .models import Product
 
 # Create your views here.
 class ProductListView(ListView):
-    model = Product
     context_object_name = "product_list"
     template_name = "products/list.html"
+
+    def get_queryset(self, *args,**kwargs):
+        request = self.request
+        return Product.objects.all()
 
 def product_list_view(request):
     all_products = Product.objects.all()
@@ -17,20 +20,35 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get("pk")
+        found_product = Product.objects.get_by_id(pk)
+        print(found_product)
+        if found_product is None:
+            raise Http404("No product matches.")
+
+        return found_product
 
     #product_detail = get_object_or_404(Product, pk=pk)
+    #try:
+    #    product_detail = Product.objects.get(pk=pk)
+    #except Product.DoesNotExist:
+    #    print("No product matches query")
+    #    raise Http404("No product matches the given search.")
 def product_detail_view(request, pk):
-    try:
-        product_detail = Product.objects.get(pk=pk)
-        context = {"product": product_detail}
-    except Product.DoesNotExist:
-        print("No product matches query")
-        raise Http404("No product matches the given search.")
+
+    found_product = Product.objects.get_by_id(pk)
+    print(found_product)
+    if found_product is None:
+        raise Http404("No product matches.")
+    context = {"product": found_product}
     return render(request, "products/detail.html", context)
