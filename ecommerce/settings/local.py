217a6c142889d23d7427fc56de2d2b8ca30ab4b4
jonathanespana <tempe.espana@gmail.com>
2024-04-25 13:26:57 -0600

    updated allowed hosts

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -36,7 +36,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.udemy-dj-ecomm.onrender.com"]
 
 
 # Application definition
